896fb76851f0fec2e0698dd831590459528a566d
sxu <bnsxu@naver.com>
2024-07-17 17:09:45 +0900

    프로젝트 생성, 상품관리 오류

--- GoodsDAO.java
+++ GoodsDAO.java
@@ -1,9 +1,11 @@
-class GoodsDAO 
+class  GoodsDAO
 {
 	public void insert(){
 		System.out.println("insert goods OK");
 	}
+
 	public void update(){
 		System.out.println("update goods OK");
 	}
+
 }
